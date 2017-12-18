cded770fefc23d96083f7b2daa63905bfd8e36d6
Alexander Kurakin <kurakin.alexander.a@gmail.com>
2017-12-19 00:14:17 +0300

    movement and so on

---
+++ Galaxy/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+
+        float horizontalInput = Input.GetAxis("horizontal");
+        float verticalInput   = Input.GetAxis("vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.up    * Time.deltaTime * speed * verticalInput);
+
+        
+    }
+}
